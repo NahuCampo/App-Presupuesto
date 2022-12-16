04319e3dbdb76daf2d0eda29e512cd56c1b7dfe9
Nahuel Campo <nahuel9567@gmail.com>
2022-12-16 16:16:50 -0300

    Seccion egresos e ingresos dinamica con JS

--- js/app.js
+++ js/app.js
@@ -1,15 +1,17 @@
 const ingresos = [
-    new Ingreso('Salario', 2100.00),
-    new Ingreso('Monotributo', 1500)
+    new Ingreso('Salario', 60000.00)
 ];
 
 const egresos = [
-    new Egreso('Comida', 800.00),
-    new Egreso('Impuestos', 2500)
+    new Egreso('Alquiler', 47000),
+    new Egreso('Impuestos', 5000),
+    new Egreso('Comida',5000)
 ];
 
 let cargarApp = () =>{
     cargarCabecero();
+    cargarIngresos();
+    cargarEgresos();
 }
 
 let totalIngresos = () =>{
@@ -46,4 +48,55 @@ const formatoMoneda = (valor) =>{
 const formatoPorcentaje = (valor) =>{
     return valor.toLocaleString('es-AR',{style: 'percent', minimumFractionDigits: 2});
 
+}
+
+const cargarIngresos = () =>{
+    let ingresosHTML = '';
+    for(let ingreso of ingresos){
+        ingresosHTML += crearIngresoHTML(ingreso);
+    }
+    document.getElementById('lista-ingresos').innerHTML = ingresosHTML
+}
+
+const crearIngresoHTML = (ingreso) =>{
+    let ingresoHTML = `
+    <div class="elemento limpiarEstilos">
+        <div class="elemento_descripcion">${ingreso.descripcion}</div>
+        <div class="derecha limpiarEstilos">
+            <div class="elemento_valor">${formatoMoneda(ingreso.valor)}</div>
+            <div class="elemento_porcentaje">${formatoPorcentaje(ingreso.valor / totalIngresos())}</div>
+            <div class="elemento_eliminar">
+                <button class="elemento_eliminar--btn">
+                    <ion-icon name="close-circle-outline"></ion-icon>
+                </button>
+            </div>
+        </div>
+    </div>
+    `;
+    return ingresoHTML;
+}
+const cargarEgresos = () =>{
+    let egresosHTML = '';
+    for(let egreso of egresos){
+        egresosHTML += crearEgresoHTML(egreso);
+    }
+    document.getElementById('lista-egresos').innerHTML = egresosHTML
+}
+
+const crearEgresoHTML = (egreso) =>{
+    let egresoHTML = `
+    <div class="elemento limpiarEstilos">
+        <div class="elemento_descripcion">${egreso.descripcion}</div>
+        <div class="derecha limpiarEstilos">
+            <div class="elemento_valor">${formatoMoneda(egreso.valor)}</div>
+            <div class="elemento_porcentaje">${formatoPorcentaje(egreso.valor / totalEgresos())}</div>
+            <div class="elemento_eliminar">
+                <button class="elemento_eliminar--btn">
+                    <ion-icon name="close-circle-outline"></ion-icon>
+                </button>
+            </div>
+        </div>
+    </div>
+    `;
+    return egresoHTML;
 }
